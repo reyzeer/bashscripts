f40820ae040cd02f2775d2ef61102058c172d931
reyzeer <czarneckizbigniew0@gmail.com>
2016-06-02 10:29:20 +0200

    Installer: WebDev - Update

--- apps/webdev.bash
+++ apps/webdev.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 apt-get install apache2
+#Apache2 Rewrite Mod
+a2enmod rewrite
+
 apt-get install mysql-server
 apt-get install mysql-workbench
-apt-get install php php-curl php-intl php-dom php-xdebug
+apt-get install php
+apt-get install php-curl
+apt-get install php-intl
+apt-get install php-dom
+apt-get install php-xdebug
+apt-get install libapache2-mod-auth-mysql
+apt-get install php-mysql
+apt-get install libapache2-mod-php
+apt-get install php-mcrypt
+apt-get install php-mbstring
 
