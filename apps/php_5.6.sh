032ef4409d3b5eba137361cde47d77dd5a2d8f7f
Zbigniew Czarnecki <reyzeer>
2017-02-22 09:47:24 +0100

    Updated php5.6 installer

--- apps/php_5.6.sh
+++ apps/php_5.6.sh
@@ -3,22 +3,4 @@
 add-apt-repository ppa:ondrej/php
 apt-get update
 
-apt-get install php5.6
-
-#sudo a2dismod php5.6
-#sudo a2enmod php7.0
-#sudo service apache2 restart
-
-apt-get install php5.6-xml
-apt-get install php5.6-simplexml
-apt-get install php5.6-mysql
-apt-get install php5.6-mysqli
-apt-get install php5.6-mbstring
-apt-get install php5.6-gd
-apt-get install php5.6-curl
-apt-get install php5.6-xdebug
-
-apt-get install imagemagick
-apt-get install php5.6-imagick
-
-service apache2 restart
+apt-get install -y php5.6 php5.6-xml php5.6-simplexml php5.6-mysql php5.6-mysqli php5.6-mbstring php5.6-gd php5.6-curl php5.6-xdebug
